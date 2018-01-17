0229c3f7c61084c2f69ae7fc540a73636c241d07
ZlaDaniel <danni539953@gmail.com>
2018-01-17 10:04:41 +0000

    Update forrit.py

--- forrit.py
+++ forrit.py
@@ -2,9 +2,7 @@ from bottle import route, run
 
 @route("/")
 def index():
-    return "<a href = '/tegund' >Tegund</a><br>" \
-           "<a href = '/árgerð' >Árgerð</a><br>" \
-           "<a href = '/bílnúmer' >Bílnúmer</a><br>"
+    return "<a href = '/tegund' >Tegund</a><br>" "<a href = '/árgerð' >Árgerð</a><br>" "<a href = '/bílnúmer' >Bílnúmer</a><br>"
 
 @route("/tegund")
 def bottle():
